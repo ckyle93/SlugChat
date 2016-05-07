ec9d96033d6d3abc453cb14e5a1a9760fe96e563
Joseph Faulkner <jofaulkn@ucsc.edu>
2016-05-06 23:10:17 -0700

    Finished Main app page with polling function

--- static/js/mainApp.js
+++ static/js/mainApp.js
@@ -1,29 +1,39 @@
 var currentPage = 1;
 
 function start(){
+
 if(PDFObject.supportsPDFs){
    console.log("Yay, this browser supports inline PDFs.");
 } else {
    console.log("Boo, inline PDFs are not supported by this browser");
 }
+$("#takePollDisplay").toggle();
 changePage(1);
 setFrameOnAdjust();
 
 }
 
+function togglePoll(){
+  $("#mainAppDisplay").toggle();
+  $("#takePollDisplay").toggle();
+  resetPoll(); 
+}
+
 function changePage(number){
 var numval = String(number);
-PDFObject.embed("../static/Planning.pdf", "#presentationFrame",{page: numval, height:"450px",width:"100%" });
+PDFObject.embed("../static/Planning.pdf", "#presentationFrame",{page: numval,height:"400px",width:"100%" });
 }
 
 function prevSlide(){
 currentPage = currentPage - 1;
 changePage(currentPage);
+setFrameOnAdjust();
 }
 
 function nextSlide(){
 currentPage = currentPage + 1;
 changePage(currentPage);
+setFrameOnAdjust();
 }
 
 function togglePanelBar(){
@@ -35,9 +45,9 @@ function togglePanelBar(){
   console.log($("#bottomControlPanel").height());
 
   if (panelHeight < 50){
-      setFrameHeight(windowHeight - 389 - 160);
+      setFrameHeight(windowHeight - 160);
   } else {
-      setFrameHeight(windowHeight - 40 - 160);
+      setFrameHeight(windowHeight  - 160);
   }
 }
 
@@ -45,14 +55,24 @@ function setFrameOnAdjust(){
   console.log("toggleCalled");
   var windowHeight = $(window).height();
   var panelHeight = $("#bottomControlPanel").height();
-  var newHeight = windowHeight - panelHeight;
+  //var newHeight = windowHeight - panelHeight;
   console.log($(window).height());
   console.log($("#bottomControlPanel").height());
-  setFrameHeight(windowHeight - panelHeight - 160);
+  setFrameHeight(windowHeight - 135);
+  windowHeight = $(window).height();
+  setChatHeight( windowHeight - 625);
 }
+
+
 
 function setFrameHeight(number){
     console.log('setFrame: ',number);
   $("#presentationFrame > .pdfobject").height(number);
 //  console.log($("#presentationFrame > .pdfobject").height());
 }
+
+function setChatHeight(number){
+    console.log('setChat: ',number);
+    $("#chatapp-im-disp").height(number);
+}
+$(window).resize(setFrameOnAdjust);
