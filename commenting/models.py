415a6f273da5792202be525c3b9db4cd267d0107
AramisM <amarden@ucsc.edu>
2016-06-05 18:27:48 -0700

    move comment model and form into fileManager

--- commenting/models.py
+++ commenting/models.py
@@ -11,18 +11,18 @@ class File(models.Model):
         return self.file
 
 #the real Comment table. must change referenced table.
-class Comment(models.Model):
+#class Comment(models.Model):
     #change the following line to reference whatever table is desired
-    file = models.ForeignKey(FileDB, on_delete=models.CASCADE)
+    #file = models.ForeignKey(FileDB, on_delete=models.CASCADE)
     
     #the comment itself, simple text field
-    comment = models.CharField(max_length=200)
+    #comment = models.CharField(max_length=200)
     
     #creates a reference to the user who made the comment
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     #pub_date is auto pulled. make sure datetime import is present
-    pub_date = models.DateTimeField(default=datetime.now, blank=True)
+    #pub_date = models.DateTimeField(default=datetime.now, blank=True)
     
-    def __str__(self):
-        return self.comment
+    #def __str__(self):
+    #    return self.comment
--- fileManager/forms.py
+++ fileManager/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 from django.forms import ModelForm
 from fileManager.models import FileDB
+from fileManager.models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment']
 
 class FileForm(ModelForm):
 	class Meta:
