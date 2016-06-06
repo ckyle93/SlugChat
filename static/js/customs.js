3ebb77e16a0f379800f0276a29327db7d0b6fdfe
MarvinCorro <corro.marvin@gmail.com>
2016-06-05 19:40:28 -0700

    fix some stuff

---
+++ static/js/customs.js
@@ -0,0 +1,18 @@
+/*
+function sendData()
+{
+	var arr = [ $("#getAbout").val(), $("#getEmail").val(), $("#getSchool").val(), $("#getStudentId").val(), $("#getStatus").val()];
+	return arr;
+
+}
+
+
+$(document).ready(function()
+	{
+		$(#sendInfo).click(function()
+		{
+
+		})
+	})
+
+*/
