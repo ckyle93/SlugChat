1d77c02bd39646721b8edd32dd9c6f9aae94df57
Chris Kyle <ckyle@ucsc.edu>
2016-08-28 15:45:12 -0700

    Implement basic quiz functionality

--- class/views.py
+++ class/views.py
@@ -1,8 +1,8 @@
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from slugchat.functions import logged_in, QuizObj
-# from home.models import Course
+from home.models import Course
 from home.models import Quiz
 
 
@@ -26,39 +26,23 @@ def class_chat(request):
     status = user.get_status()
     if status == 'Professor':
         quizzes = user.quiz_set.all().all()
-        quiz_list = []
-        for quiz in quizzes:
-            new_quiz = QuizObj()
-            new_quiz.question_text = quiz.question
-            new_quiz.id = quiz.id
-            choices = quiz.quizchoices_set.all().all()
-            for choice in choices:
-                new_quiz.choices.append(choice.choice)
-            quiz_list.append(new_quiz)
-    else:
-        quiz_list = []
 
     context = {'firstname': user.firstName, 'status': status,
-               'quiz_list': quiz_list}
+               'quiz_list': quizzes}
     return render(request, 'class/quiz_test.html', context)
 
 
 @csrf_exempt
 def pick_quiz(request):
     quizID = request.POST['quizID']
-    class_title = request.POST['class']
+    # className = request.POST['class']
 
     quiz = Quiz.objects.get(id=quizID)
+    # We may not actually need the class name. TBD
+    # Also it doesn't seem to work correctly right now.
+    # course = Course.objects.get(title=className)
+    context = {'quiz': quiz}
 
-    new_quiz = QuizObj()
-    new_quiz.question_text = quiz.question
-    new_quiz.id = quiz.id
-    choices = quiz.quizchoices_set.all().all()
-    for choice in choices:
-        new_quiz.choices.append(choice)
-    context = {'quiz': new_quiz}
-
-    course = Course.objects.get(title=class_title)
     return render(request, 'class/show_quiz.html', context)
 
 
@@ -81,7 +65,7 @@ def quiz_choices(request):
     if user is None:
         return HttpResponseRedirect('/')
 
-    currentclass = request.get.get('class', '')
+    currentclass = request.GET.get('class', '')
 
     if currentclass == '':
         return HttpResponseRedirect('/')
@@ -108,35 +92,37 @@ def quiz_choices(request):
 
 @csrf_exempt
 def check_quiz(request):
-    user = logged_in(request)
+    return HttpResponse("hello", 400)
+#     user = logged_in(request)
+# 
+#     if user is None:
+#         return HttpResponseRedirect('/')
+# 
+#     # currentclass = request.GET.get('class', '')
+# 
+#     if currentclass == '':
+#         return HttpResponseRedirect('/')
+# 
+#     if not Course.objects.filter(title=currentclass).exists():
+#         return HttpResponseRedirect('/')
+# 
+#     if not Course.objects.get(title=currentclass, activeQuiz=True):
+#         return HttpResponse('')
+# 
+#     course = Course.objects.get(title=currentclass)
+#     quizID = course.quizID
+# 
+#     quiz = Quiz.objects.get(id=quizID)
+# 
+#     new_quiz = QuizObj()
+#     new_quiz.question_text = quiz.question
+#     new_quiz.id = quiz.id
+#     choices = quiz.quizchoices_set.all().all()
+#     for choice in choices:
+#         new_quiz.choices.append(choice)
+#     context = {'quiz': new_quiz}
+#     return render(request, 'class/show_quiz.html', context)
 
-    if user is None:
-        return HttpResponseRedirect('/')
-
-    currentclass = request.get.get('class', '')
-
-    if currentclass == '':
-        return HttpResponseRedirect('/')
-
-    if not Course.objects.filter(title=currentclass).exists():
-        return HttpResponseRedirect('/')
-
-    if not Course.objects.get(title=currentclass,activeQuiz=True):
-        return HttpResponse('')
-
-    course = Course.objects.get(title=currentclass)
-    quizID = course.quizID
-
-    quiz = Quiz.objects.get(id=quizID)
-
-    new_quiz = QuizObj()
-    new_quiz.question_text = quiz.question
-    new_quiz.id = quiz.id
-    choices = quiz.quizchoices_set.all().all()
-    for choice in choices:
-        new_quiz.choices.append(choice)
-    context = {'quiz': new_quiz}
-    return render(request, 'class/show_quiz.html', context)
 
 
 # def give_quiz(request):
